72b04b9e600c3f4bdb232f42f2483ecfff517a43
thiagogilcamargo <thiagogilcmargo@gmail.com>
2023-08-31 21:59:45 -0300

    corrigindo rotas!

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
-
 import ReactDOM from 'react-dom/client'
-
 import App from './App.jsx'
-
 import './index.css'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './routes/Home.jsx';
+import Produtos from './routes/Produtos.jsx';
+import EditarProdutos from './routes/EditarProdutos.jsx';
+import Error from './routes/Error.jsx';
+
+const router = createBrowserRouter([
+  {path: "/" , element: <App/>, errorElement: <Error/>,
+    children:[
+      {path: "/" , element: <Home/>},
+      {path: "/produtos" , element: <Produtos/>},
+      {path: "/produtos/editar/:id" , element: <EditarProdutos/>},
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
-
-
