7a74e0e19e65f3c92f08b51123279831bc1fda6d
thiagogilcamargo <thiagogilcmargo@gmail.com>
2023-08-31 21:59:33 -0300

    corrigindo rotas !

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,14 @@
-import reactLogo from "./assets/react.svg";
-import Cabecalho from "./components/Cabecalho";
-import Conteudo from "./components/Conteudo";
-import Rodape from "./components/Rodape";
+import Navbar from "./components/Navbar";
+import { Outlet, useRoutes } from "react-router-dom";
 
 export default function App() {
   
-  //ÁREA DECLARATIVA
 
-  let reactLogoTextoAlt = "Logo do React";
 
   return (
     <>
-      {/* Área imperativa! */}
-      <div>
-        {/* Um cabeçalho com um header, um h1, uma lista ul com 3 itens com 3 links. */}
-      <Cabecalho/>
-        {/* Uma seção com uma div com 3 parágrafos, com 3 linhas de lorem, uma imagem */}
-
-      <Conteudo reactLogoProps={reactLogo} reactLogoTextoAltProps={reactLogoTextoAlt} />
-        
-        {/* Um rodapé com uma div, uma lista ul com 3 itens e links para redes sociais */}
-        <Rodape/>
-
-      </div>
+      <Navbar/>
+      <Outlet/>
     </>
   )
 }
