90915ab1ab9b07d965fab884d4a713b3a93ce681
thiagogilcamargo <thiagogilcmargo@gmail.com>
2023-10-03 21:56:39 -0300

    estilizando o cabecalho

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Outlet} from "react-router-dom";
-import Cabecalho from "./components/Cabecalho";
+import Cabecalho from "./components/Cabecalho/Cabecalho";
 import Rodape from "./components/Rodape";
 
 export default function App() {
--- src/components/Cabecalho/Cabecalho.jsx
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -3,8 +3,7 @@ import { Link } from "react-router-dom";
 export default function Cabecalho() {
   return (
     <>
-      <header>
-        <h1>Vite + React / Coded By-Thiago Gil Camargo</h1>
+      <header className="cabecalho">
         <ul>
           <li>
             <Link to="/">HOME</Link>
